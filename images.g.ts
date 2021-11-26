17f7e1a6dd1cae3d03106b14605ca039b9a85aad
MrPlumpOnGitHub <95065587+MrPlumpOnGitHub@users.noreply.github.com>
2021-11-26 16:06:32 +1100

    Update pxt.json, main.blocks, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,27 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Ball":return img`
+. . 6 6 6 6 . . 
+. 6 d 4 4 4 6 . 
+6 1 b 1 1 4 d 6 
+c 1 b b 4 4 1 c 
+. c b b b d c . 
+. . c c c c . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
